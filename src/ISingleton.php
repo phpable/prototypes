45da1ef4781562cf0d64cbbce84aed32859076d9
hacpaka <hacpaka@eggbe.com>
2016-05-11 21:39:36 +0300

    +

--- src/ISingleton.php
+++ src/ISingleton.php
@@ -4,8 +4,8 @@ namespace Eggbe\Prototype;
 interface ISingleton {
 
 	/**
-	 * This is just an alternative name for the traditional getInstance method.
-	 * We had serious reason to rename it because the traditional name is too long at least.
+	 * This is just the alternative name for the traditional getInstance method.
+	 * We had serious reason to rename it because the traditional name is too long.
 	 *
 	 * @return ISingleton
 	 */
