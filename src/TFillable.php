78459a78e574dc788e76d958748d2d1d968b23dc
hacpaka <hacpaka@eggbe.com>
2017-04-15 09:46:45 +0300

    +

---
+++ src/TFillable.php
@@ -0,0 +1,21 @@
+<?php
+namespace Eggbe\Prototype;
+
+trait TFillable {
+
+	/**
+	 * Fill object values via array.
+	 * @param array $Values
+	 * @throws \Exception
+	 */
+	public function fill(array $Values) {
+		foreach ($Values as $name => $value){
+			if (!property_exists($this, $name)){
+				throw new \Exception('Undefined property "' . $name . '"!');
+			}
+
+			$this->{$name} = $value;
+		}
+	}
+
+}
