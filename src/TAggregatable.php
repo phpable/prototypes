5028db05f6d52486aa265f25e9f5e93beffb397c
hacpaka <hacpaka@eggbe.com>
2017-07-24 07:54:11 +0300

    +

--- src/TAggregatable.php
+++ src/TAggregatable.php
@@ -14,7 +14,7 @@ trait TAggregatable {
 	 * @return array
 	 */
 	protected static function aggregate(string $name) : array {
-		return array_merge(Src::call([get_parent_class(static::class), 'aggregate'], $name, []),
-			Arr::cast(Arr::get(get_class_vars(static::class), $name)));
+		return array_merge(is_callable($target = [get_parent_class(static::class), 'aggregate'])
+			? call_user_func($target, $name) : [], Arr::cast(Arr::get(get_class_vars(static::class), $name)));
 	}
 }
