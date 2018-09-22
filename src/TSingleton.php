62f7ef5c7e1098bf443f24a62eb6655bec2f80b1
hacpaka <hacpaka@eggbe.com>
2018-09-23 00:20:08 +0300

    critical bug fixed

--- src/TSingleton.php
+++ src/TSingleton.php
@@ -15,7 +15,7 @@ trait TSingleton {
 	 */
 	public static function make(): ISingleton {
 		if (!array_key_exists(static::class, self::$Instances)){
-			self::$Instances[static::class] = new static(...func_num_args());
+			self::$Instances[static::class] = new static(...func_get_args());
 		}
 
 		return self::$Instances[static::class];
