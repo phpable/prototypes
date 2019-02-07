e8da3139c5d6b3e7f6eb671eeb7434060a767913
hacpaka <hacpaka@eggbe.com>
2019-02-07 08:05:54 +0300

    +

--- src/TTraitable.php
+++ src/TTraitable.php
@@ -2,18 +2,24 @@
 namespace Able\Prototypes;
 
 use \Able\Helpers\Src;
+use \Able\Helpers\Arr;
+
+use \Generator;
 
 trait TTraitable {
 
 	/**
 	 * @param string $prefix
+	 * @param mixed ...$args
+ 	 * @return Generator
 	 */
-	protected final function propagate(string $prefix): void {
-		foreach(class_uses($this) as $trait){
-			if (method_exists($this, $method = $prefix . Src::rns($trait))) {
-				$this->{$method}();
-			}
+	protected final function propagate(string $prefix, ...$args): Generator {
+		foreach (array_filter(Arr::make(Src::traits($this), function($_) use ($prefix ) {
+			return sprintf('%s%s', $prefix, Src::unpref($_));
+		}), function($_){
+			return method_exists($this, $_);
+		}) as $name => $method) {
+			yield $name => $this->{$method}(...$args);
 		}
 	}
-
 }
