6d6ea35db0accef437b2ce3e868b6135ea5378d1
Andrew <hacpaka@kamora.vn>
2021-05-08 12:16:45 +0700

    +

--- src/TRetrospective.php
+++ src/TRetrospective.php
@@ -14,9 +14,10 @@ trait TRetrospective {
 	 * @param string $name
 	 * @return array
 	 */
-	protected final static function retrospect(string $name){
+	protected final static function retrospect(string $name): array {
 		return call_user_func_array('array_merge', array_map(function($class) use ($name){
 			return property_exists($class, $name) ? $class::${$name} : [];
+
 		}, array_reverse(static::hierarchy())));
 	}
 }
--- src/TUnclonable.php
+++ src/TUnclonable.php
@@ -1,13 +1,14 @@
 <?php
 namespace Able\Prototypes;
 
+use \Exception;
+
 trait TUnclonable {
 
 	/**
-	 * @throws \Exception
+	 * @throws Exception
 	 */
 	public final function __clone(){
-		throw new \Exception(sprintf('Can not clone an unclonable object of the class: %s!', static::class));
+		throw new Exception(sprintf('Can not clone an unclonable instance of the class: %s!', static::class));
 	}
-
 }
