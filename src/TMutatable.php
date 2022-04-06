32b88d7c44569be7fe7b202f28105984c1df3dbc
Andrew Kress <hacpaka@kamora.vn>
2022-04-07 00:50:50 +0700

    +

--- src/TMutatable.php
+++ src/TMutatable.php
@@ -8,21 +8,27 @@ trait TMutatable {
 
 	/**
 	 * Checks if mutator exists and returns the mutated value.
-	 *
-	 * @param string $prefix
-	 * Any prefix like 'get' or 'set' to separate getters and setters for example.
-	 *
-	 * @param string $name
-	 * It will be camelcased considering an underscore like a separator.
-	 *
-	 * @param $value
-	 * Some parameter if needed.
-	 *
-	 * @return mixed
-	 * The value will be returned "as is" if the requested mutator doesn't exist.
 	 */
-	protected final function mutate(string $prefix, string $name, mixed $value = null): mixed {
-		return method_exists($this, $method = Src::tcm(strtolower(Str::join('_', $prefix, $name , 'property'))))
-			? call_user_func([$this, $method], $value) : $value;
+	protected final function mutate(string $prefix, string $name, mixed $value): mixed {
+		return method_exists($this,
+
+			/**
+			 * The method name will be camelcased
+			 * considering an underscore like a separator.
+			 */
+			$method = Src::tcm(strtolower(Str::join('_', $prefix, $name,
+
+				/**
+				 * The suffix is always static.
+				 */
+				'property'))))
+
+		? call_user_func([$this, $method], $value)
+
+		/**
+		 * The value will be returned "as is"
+		 * if the requested mutator doesn't exist.
+		 */
+		: $value;
 	}
 }
